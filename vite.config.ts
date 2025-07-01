1e08abf116f4d301ca7c4d71736b7470d178fc65
Ray Pulido <ray_escom@outlook.com>
2025-06-30 21:14:18 -0600

    Add Biome and Husky configuration

--- vite.config.ts
+++ vite.config.ts
@@ -1,7 +1,24 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { resolve } from "node:path";
+import tailwindcss from "@tailwindcss/vite";
+import react from "@vitejs/plugin-react";
+import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
-})
+  plugins: [react(), tailwindcss()],
+  server: {
+    port: 8000,
+  },
+  resolve: {
+    alias: {
+      "@": resolve("./src"),
+      "@components": resolve("./src/components"),
+      "@hooks": resolve("./src/hooks"),
+      "@screens": resolve("./src/screens"),
+      "@utils": resolve("./src/utils"),
+      "@service": resolve("./src/service"),
+      "@assets": resolve("./src/assets"),
+      "@i18n": resolve("./src/i18n"),
+    },
+  },
+});
